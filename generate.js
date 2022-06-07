2cef211dfdf166197336857a8b679fa4f8b4f678
Soumya <soumya.mobileautomation@gmail.com>
2022-06-08 00:33:48 +0530

    Update generate.js

--- generate.js
+++ generate.js
@@ -435,11 +435,26 @@ function displayCalc(container, type, params) {
         var header = table.createTHead();
         var header_row = header.insertRow(0);
         var header_cell1 = header_row.insertCell(0);
-
-//         var medInfoHeading = document.createElement("h2");
-//         medInfoHeading.textContent = "Calclator Name: " + type;
         header_cell1.innerHTML = "<h2>Calclator Name: " + type + "</h2>";
         calcTableDiv.appendChild(table);
+  
+          var row1 = table.insertRow(-1);
+        var row1Cell = row1.insertCell(-1);
+//         var medInfoHeading3 = document.createElement("h3");
+//         medInfoHeading3.textContent = "Formula: " + paramList.join("");
+        row1Cell.appendChild(document.createTextNode("Formula: " + paramList.join("")));
+
+        // var row2 = table.insertRow(-1);
+        // var row2Cell = row2.insertCell(-1);
+        // var resultH1 = document.createElement("h3");
+        // resultH1.textContent = "Age= 35, AST=40, ALT=32, Platelet=300";
+        // row2Cell.appendChild(document.createTextNode(resultH1));
+
+        // var row3 = table.insertRow(-1);
+        // var row3Cell = row3.insertCell(-1);
+        // var resultH3 = document.createElement("h2");
+        // resultH3.textContent = "Result= " + "0.82 points"; 
+        // row3Cell.appendChild(document.createTextNode(resultH3));
   
 //         var calcDiv = document.createElement("div");
 //         calcTableDiv.appendChild(calcDiv);
